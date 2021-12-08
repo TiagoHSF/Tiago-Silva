b1bb898b1cb6f5e71a5e0b1b905a79845bb4cf67
Tiago Henrique da Silva <tiagohs.hotmart@gmail.com>
2021-12-08 00:01:59 -0300

    Sweet Alert, botoes e css

--- src/app/sobre-mim/sobre-mim.component.ts
+++ src/app/sobre-mim/sobre-mim.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import Swal from 'sweetalert2'
+import 'sweetalert2/src/sweetalert2.scss'
 @Component({
   selector: 'app-sobre-mim',
   templateUrl: './sobre-mim.component.html',
@@ -10,5 +11,29 @@ export class SobreMimComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  abrirModalIDE(){
+    Swal.fire({
+      title: "CONHECIMENTO EM IDES",
+      confirmButtonText: "Fechar",
+      text: "Visual Studio Code, IntelliJ, Eclipse"
+    })
+  }
+
+  abrirModalLinguagens(){
+    Swal.fire({
+      title: "LINGUAGENS DE PROGRAMAÇÃO",
+      confirmButtonText: "Fechar",
+      text: "TypeScript, Java, SQL"
+    })
+  }
+
+   abrirModalAws(){
+    Swal.fire({
+      title: "INFRAESTRUTURA",
+      confirmButtonText: "Fechar",
+      text: "EC2, S3, ELB, RDS, SQS, SNS, SES, REDIS"
+    })
   }
 }
